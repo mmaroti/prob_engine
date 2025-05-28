16e29624d3a88fe7de00ba33c04ecfdcadd8ad86
Daniel Bezdany <bezdanydaniel@gmail.com>
2025-05-28 17:16:08 +0200

    Partial implementation of multivariate normal distribution.

--- src/prob_engine/multi_normal.py
+++ src/prob_engine/multi_normal.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2023, Miklos Maroti
+# Copyright (C) 2023, Miklos Maroti and Daniel Bezdany
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -43,12 +43,70 @@ class MultiNormal(Distribution):
         yield self._means
         yield self._sdevs
 
+    def sample(self, batch_shape: torch.Size) -> torch.Tensor:
+        centered = ( torch.normal( 0.0, 1.0,
+                                  size = batch_shape + (self.event_numel, ) ) 
+                     * self.sdevs.flatten().abs())
+        return self.means + centered.view(batch_shape + self.event_shape)
+    
+    def get_pdf(self, sample: torch.Tensor) -> torch.Tensor:
+        assert self.sdevs.abs().prod() > 0
+        #Could implement as sdev_i=0 meaning that i-th coordinate is fixed
+        batch_shape = sample.shape[:-len(self.event_shape)]
+        assert sample.shape == batch_shape + self.event_shape
+        coeff = ( 2 * torch.tensor(torch.pi) ).pow(-self.event_numel/2.0)
+        det = self.sdevs.abs().flatten().prod()
+        exparg = -0.5 * ( ( sample.view( batch_shape + (self.event_numel, ) ) 
+                  - self.means.flatten()
+                  ).pow(2) / self.sdevs.abs().flatten() ).sum(-1)
+        return coeff * det.pow(-0.5) * torch.exp(exparg)
+
+
+    def log_prob(self, sample: torch.Tensor) -> torch.Tensor:
+        assert self.sdevs.abs().prod() > 0
+        #Could implement as sdev_i=0 meaning that i-th coordinate is fixed
+        batch_shape = sample.shape[:-len(self.event_shape)]
+        assert sample.shape == batch_shape + self.event_shape
+        coeff = 2 * torch.tensor(torch.pi)
+        detlog = self.sdevs.abs().flatten().log().sum()
+        exparg = -0.5 * ( ( sample.view( batch_shape + (self.event_numel, ) )
+                  - self.means.flatten()
+                  ).pow(2) / self.sdevs.flatten().abs() ).sum(-1)
+        return ( - self.event_numel * coeff.log() / 2.0 
+                 - 0.5 * detlog + exparg )
+    
+    def get_cdf(self, sample: torch.Tensor) -> torch.Tensor:
+        assert self.sdevs.abs().prod() > 0
+        #Could implement as sdev_i=0 meaning that i-th coordinate is fixed
+        batch_shape = sample.shape[:-len(self.event_shape)]
+        assert sample.shape == batch_shape + self.event_shape
+        if self.event_numel == 1:
+            sq2 = torch.sqrt( torch.tensor(2) )
+            arg = ( ( sample.view(batch_shape + (self.event_numel, )) 
+                     - self.means.flatten() ) 
+                    / ( self.sdevs.flatten().abs() * sq2 ) )
+            return ( 0.5 + 0.5 * torch.erf( arg ) )
+        elif self.event_numel == 2:
+            raise NotImplemented()
+        else:
+            raise NotImplemented()
+
 
 def test():
-    dist = MultiNormal(
-        torch.tensor(0.0),
-        torch.tensor(1.0))
-    print(dist.event_shape)
-    # dist.plot_exact_density()
-    dist.plot_empirical_pdf()
-    # dist.plot_sample_cumulative()
+    dist1 = MultiNormal( torch.tensor([0.0]), torch.tensor([0.5]) )
+    print("Event shape", dist1.event_shape)
+    print("Parameters", list(dist1.parameters))
+    dist1.plot_empirical_pdf()
+    dist1.plot_exact_pdf()
+    dist1.plot_empirical_cdf()
+    dist1.plot_exact_cdf()
+
+    dist2 = MultiNormal(
+        torch.tensor([0.0, 0.0]),
+        0.5*torch.tensor([1.0, 0.3]))
+    print("Event shape", dist2.event_shape)
+    print("Parameters", list(dist2.parameters))
+    dist2.plot_empirical_pdf()
+    dist2.plot_exact_pdf()
+    dist2.plot_empirical_cdf()
+    dist2.plot_exact_cdf()
